f0d5fd11c711ee9293b9d23a39e97eba7aa63f8d
Volker Knollmann <v.knollmann@web.de>
2016-10-17 00:50:57 +0200

    Added a build-script for the Debian-based OVH box

---
+++ dist/build_OVH.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PKGVER=0.1.0
+PKGRELEASE=1
+PKGNAME=libSloppy
+BUILDDIR=/tmp/$PKGNAME-build
+
+STARTDIR=$(pwd)
+
+mkdir $BUILDDIR
+pushd $BUILDDIR
+
+git clone -b $PKGVER --single-branch --depth 1 file:///srv/git/libSloppy.git
+cd libSloppy
+mkdir release
+cd release
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make
+echo "A C++ lib of sloppyly implemented helper functions" > description-pak
+checkinstall --pkgversion $PKGVER \
+	--pkgrelease $PKGRELEASE \
+	--pkgname $PKGNAME \
+	--provides $PKGNAME \
+	--install=no \
+	--nodoc \
+	-y
+
+mv *deb $STARTDIR
+
+popd
+
+rm -rf $BUILDDIR
+

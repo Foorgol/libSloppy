4da3177aa01d337d07e46086c72e7c390717b84a
Volker Knollmann <v.knollmann@web.de>
2016-12-06 19:42:37 +0100

    Added a function for returning substrings delimited by indices

--- Sloppy/libSloppy.h
+++ Sloppy/libSloppy.h
@@ -62,6 +62,38 @@ namespace Sloppy
     boost::trim(s);
     return (maxLen > 0) ? (!(s.empty() || (s.length() > maxLen))) : (!(s.empty()));
   }
+
+  // replace a section in a string with another string
+  inline bool replaceStringSection(string& data, size_t startIdxToDelete, size_t endIdxToDelete, const string& replacement)
+  {
+    if (endIdxToDelete < startIdxToDelete) return false;
+
+    if ((startIdxToDelete > (data.size() - 1)) || (endIdxToDelete > (data.size() - 1))) return false;
+
+    string part1;
+    if (startIdxToDelete > 0)
+    {
+      part1 = data.substr(0, startIdxToDelete);
+    }
+
+    string part2;
+    if (endIdxToDelete < (data.size() - 1))
+    {
+      part2 = data.substr(endIdxToDelete + 1);
+    }
+
+    data = part1 + replacement + part2;
+
+    return true;
+  }
+
+  // get a slice of a string delimited by two indices
+  inline string getStringSlice(const string& s, size_t idxStart, size_t idxEnd)
+  {
+    if (idxEnd < idxStart) return "";
+
+    return s.substr(idxStart, idxEnd - idxStart + 1);
+  }
 }
 
 #endif

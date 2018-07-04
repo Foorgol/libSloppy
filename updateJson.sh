9326bad063990fd560a075a30649e7d6dfe00720
Volker Knollmann <v.knollmann@web.de>
2018-07-04 20:29:04 +0200

    Changed the updater to retrieve the files from "master" and not from "debug". Added retrieval of the forward header.

--- updateJson.sh
+++ updateJson.sh
@@ -6,12 +6,21 @@ echo "Niels Lohmann's header-only JSON implementation"
 echo and committing it to the repository
 echo
 
-curl https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp > json.hpp
+curl https://raw.githubusercontent.com/nlohmann/json/master/single_include/nlohmann/json.hpp > json.hpp
 if [ $? -ne 0 ]; then
   echo
   echo "!!! Download failed !!!"
   echo
   exit 1
 fi
-git add json.hpp
-git commit -m "Update of json.hpp from Github" json.hpp
+
+curl https://raw.githubusercontent.com/nlohmann/json/master/include/nlohmann/json_fwd.hpp > json_fwd.hpp
+if [ $? -ne 0 ]; then
+  echo
+  echo "!!! Download failed !!!"
+  echo
+  exit 1
+fi
+
+git add json.hpp json_fwd.hpp
+git commit -m "Update of json.hpp from Github" json.hpp json_fwd.hpp

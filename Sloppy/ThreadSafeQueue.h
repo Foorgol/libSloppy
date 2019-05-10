a311499594ba3680287793937726a5b9727f220b
Volker Knollmann <v.knollmann@web.de>
2019-05-10 21:16:34 +0200

    Added a queue template that can be used for safe data exchange between two threads.

---
+++ Sloppy/ThreadSafeQueue.h
@@ -0,0 +1,196 @@
+/*
+ *    This is libSloppy, a library of sloppily implemented helper functions.
+ *    Copyright (C) 2016 - 2018  Volker Knollmann
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ *    This is libSloppy, a library of sloppily implemented helper functions.
+ *    Copyright (C) 2016 - 2018  Volker Knollmann
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIBSLOPPY_THREADSAFEQUEUE_H
+#define __LIBSLOPPY_THREADSAFEQUEUE_H
+
+#include <string>
+#include <chrono>
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+#include <Sloppy/Timer.h>
+
+using namespace std;
+
+namespace Sloppy
+{
+  /** \brief Template class for a queue that can be used by independent threads,
+   * one thread for reading and one or more for writing.
+   *
+   * The reading thread is be notified through a condition variable
+   * that new data has arrived. This allows for more efficient synchronization
+   * compared to cyclic polling.
+   *
+   * The queue uses the first-in-first-out principle.
+   */
+  template<typename T>
+  class ThreadSafeQueue
+  {
+  public:
+    /** \brief Copy-append data to the end of the queue
+     */
+    void put(
+        const T& inData   ///< the data that shall be copy-appended to the queue
+        )
+    {
+      lock_guard<mutex> lg{listMutex};
+      queue.push_back(inData);
+      cv.notify_one();
+    }
+
+    /** \brief Move-append data to the end of the queue
+     */
+    void put(
+        T&& inData   ///< the data that shall be moved to the queue
+        )
+    {
+      lock_guard<mutex> lg{listMutex};
+      queue.push_back(std::move(inData));
+      cv.notify_one();
+    }
+
+    /** \brief Reads data from the queue into a caller-provided reference
+     */
+    void get(
+        T& outData   ///< the reference to copy the data to (will be transfered by copy-assignment)
+        )
+    {
+      // condition variables only work with unique_lock, not with lock_guard
+      unique_lock<mutex> lock{listMutex};
+
+      // wait for a notification that new data has arrived;
+      // ignores spurious wakeups and is guaranteed to not return
+      // until there is data in the queue
+      cv.wait(lock, [this](){ return !queue.empty(); });
+
+      outData = queue.front();
+      queue.pop_front();
+    }
+
+    /** \brief Reads data from the queue into a caller-provided reference
+     *
+     * \returns `true` if data was returned, `false` otherwise
+     */
+    bool get(
+        T& outData,   ///< the reference to copy the data to
+        int timeout_ms   /// an optional timeout after which the function returns without value if the queue remains empty (< 0 means: wait forever)
+        )
+    {
+      // special case: wait forever
+      if (timeout_ms < 0)
+      {
+        get(outData);
+        return true;
+      }
+
+      //
+      // use "wait" with a timeout
+      //
+
+      // condition variables only work with unique_lock, not with lock_guard
+      unique_lock<mutex> lock{listMutex};
+
+      Sloppy::Timer t;
+      t.setTimeoutDuration__ms(timeout_ms);
+      bool hasData = false;
+      while (!t.isElapsed())
+      {
+        hasData = cv.wait_for(lock, chrono::milliseconds{timeout_ms - t.getTime__ms()},
+                          [this](){ return !queue.empty(); });
+
+        if (hasData) break;
+
+        // sometime we might have "spurious wake-ups" before the
+        // timeout has elapsed.
+        //
+        // by using the timer we make sure that we stick to the
+        // demanded waiting time
+      }
+
+      // at this point, we either have data or a timeout
+      if (!hasData) return false;
+
+      outData = queue.front();
+      queue.pop_front();
+
+      return true;
+    }
+
+    /** \returns `true` if the queue has data available
+     *
+     * \note It is not recommended to cyclically poll this function
+     * to wait for fresh data, because each call requires getting
+     * and releasing a lock on a mutex.
+     */
+    bool hasData()
+    {
+      lock_guard<mutex> lg{listMutex};
+      return !queue.empty();
+    }
+
+    /** \returns `true` if the queue is empty
+     *
+     * \note It is not recommended to cyclically poll this function
+     * to wait for fresh data, because each call requires getting
+     * and releasing a lock on a mutex.
+     */
+    bool empty()
+    {
+      lock_guard<mutex> lg{listMutex};
+      return queue.empty();
+    }
+
+    /** \returns the number of items that are currently in the queue
+     *
+     * \note It is not recommended to cyclically poll this function
+     * to wait for fresh data, because each call requires getting
+     * and releasing a lock on a mutex.
+     */
+    int size()
+    {
+      lock_guard<mutex> lg{listMutex};
+      return queue.size();
+    }
+
+  private:
+    mutex listMutex;
+    condition_variable cv;
+    deque<T> queue;
+  };
+}
+
+#endif
---
+++ tests/tstThreadSafeQueue.cpp
@@ -0,0 +1,105 @@
+/*
+ *    This is libSloppy, a library of sloppily implemented helper functions.
+ *    Copyright (C) 2016 - 2018  Volker Knollmann
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <string>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include "../Sloppy/ThreadSafeQueue.h"
+#include "../Sloppy/Timer.h"
+
+using namespace std;
+
+TEST(ThreadSafeQueue, BasicUsage)
+{
+  Sloppy::ThreadSafeQueue<int> q;
+
+  ASSERT_EQ(0, q.size());
+  ASSERT_TRUE(q.empty());
+  ASSERT_FALSE(q.hasData());
+
+  // get with timeout on empty queue
+  static constexpr int timeoutMs = 10;
+  int data{-42};
+  Sloppy::Timer t;
+  ASSERT_FALSE(q.get(data, timeoutMs));
+  ASSERT_TRUE(t.getTime__ms() >= timeoutMs);
+  ASSERT_TRUE(t.getTime__ms() <= (timeoutMs + 1));
+  ASSERT_EQ(-42, data);  // variable has not been altered
+
+  // push some data
+  q.put(99);
+  ASSERT_EQ(1, q.size());
+  ASSERT_FALSE(q.empty());
+  ASSERT_TRUE(q.hasData());
+
+  // get the data
+  t.restart();
+  ASSERT_TRUE(q.get(data, timeoutMs));
+  ASSERT_TRUE(t.getTime__ms() < 1);
+  ASSERT_EQ(99, data);
+
+  ASSERT_EQ(0, q.size());
+  ASSERT_TRUE(q.empty());
+  ASSERT_FALSE(q.hasData());
+}
+
+//----------------------------------------------------------------------------
+
+TEST(ThreadSafeQueue, Notifications)
+{
+  Sloppy::ThreadSafeQueue<int> q;
+
+  // a lambda for filling the queue
+  // with element by element, each
+  // with a delay
+  static constexpr int elemCnt = 100;
+  static constexpr int insertionDelay_ms = 2;
+  auto filler = [&]()
+  {
+    for (int i =0; i < elemCnt; ++i)
+    {
+      q.put(i);
+      this_thread::sleep_for(chrono::milliseconds{insertionDelay_ms});
+    }
+  };
+
+  thread fillerThread(filler);
+
+  bool hasData = true;
+  int cnt = 0;
+  while (hasData)
+  {
+    Sloppy::Timer t;
+    int data{-42};
+    hasData = q.get(data, 2 * insertionDelay_ms);
+
+    if (hasData)
+    {
+      ASSERT_TRUE(t.getTime__ms() <= insertionDelay_ms);
+      ASSERT_EQ(data, cnt);
+      ++cnt;
+    } else {
+      ASSERT_TRUE(t.getTime__ms() >= (2 * insertionDelay_ms));
+      ASSERT_EQ(elemCnt, cnt);
+    }
+  }
+
+  fillerThread.join();
+}

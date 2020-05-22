ffe03a4f0bbaf33900f5789736e0fcf3c13bf127
Volker Knollmann <v.knollmann@web.de>
2020-05-22 19:07:30 +0200

    Added two missing includes that caused compilation errors with recent GCC

--- Sloppy/HTML/StyledElement.cpp
+++ Sloppy/HTML/StyledElement.cpp
@@ -16,6 +16,8 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdexcept>
+
 #include "StyledElement.h"
 
 using namespace std;
--- Sloppy/HTML/Table.cpp
+++ Sloppy/HTML/Table.cpp
@@ -16,6 +16,8 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdexcept>
+
 #include "Table.h"
 
 using namespace std;
